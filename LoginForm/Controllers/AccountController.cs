ae1df1105691359aafcd6932ec6d6bb58c211c81
Jeevendra <jeevendrasingh1711@gmail.com>
2023-11-02 23:58:00 +0530

    initial commit

---
+++ LoginForm/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using LoginForm.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+
+namespace LoginForm.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountDBEntities dc = new AccountDBEntities();
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel register)
+        {
+            if(ModelState.IsValid)
+            {
+                var reg = new Models.Login
+                {
+                    Name = register.Name,
+                    MobileNumber = register.MobileNumber,
+                    Email = register.Email,
+                    Address = register.Address,
+                    Password = register.Password,
+                };
+                dc.Logins.Add(reg);
+                dc.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+            return View(register);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = dc.Logins.SingleOrDefault(u => u.Name == login.Name);
+                if (user != null)
+                {
+                    if (user.Password == login.Password)
+                    {
+                        FormsAuthentication.SetAuthCookie(user.Name,false);
+                        return RedirectToAction("Success", "Account");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                    }
+                }
+            }
+            return View(login);
+        }
+        public ViewResult Success()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ LoginForm/Models/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginForm.Models
+{
+    public class RegisterViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "MobileNumber")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits")]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
